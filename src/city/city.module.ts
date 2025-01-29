d8c7f5f3b393a19a3fd65c243731aa615888966d
Cleiton barros <ton.barros123@hotmail.com>
2025-01-29 15:12:11 -0300

    feat: create cache controller and service

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,13 +3,11 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
-  imports: [
-    CacheModule.register({ ttl: 90000 }),
-    TypeOrmModule.forFeature([CityEntity]),
-  ],
+  imports: [CacheModule, TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
   providers: [CityService],
 })
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,35 +1,28 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
+import { CacheService } from '../cache/cache.service';
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
-    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    private readonly cacheService: CacheService,
   ) {}
 
   async getAllCityByStateId(stateId: number): Promise<CityEntity[]> {
-    const citiesCached = (await this.cacheManager.get(
-      `state_${stateId}`,
-    )) as CityEntity[];
+    const getCities = () => {
+      return this.cityRepository.find({
+        where: {
+          stateId,
+        },
+      });
+    };
 
-    if (citiesCached) {
-      return citiesCached;
-    }
-
-    const cities = await this.cityRepository.find({
-      where: {
-        stateId,
-      },
-    });
-
-    await this.cacheManager.set(`state_${stateId}`, cities);
-
-    return cities;
+    return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () =>
+      getCities(),
+    );
   }
 }
