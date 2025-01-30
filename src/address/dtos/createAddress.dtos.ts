f56a73ecf0dd5df0e50870ee531ea5b3919b7592
Cleiton barros <ton.barros123@hotmail.com>
2025-01-30 20:55:53 -0300

    feat: create adress controller and service

--- src/address/dtos/createAddress.dtos.ts
+++ src/address/dtos/createAddress.dtos.ts
@@ -1,6 +1,16 @@
-export interface CreateAddressDto {
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  @IsOptional()
   complement: string;
+
+  @IsInt()
   numberAddress: number;
+
+  @IsString()
   cep: string;
+
+  @IsInt()
   cityId: number;
 }
