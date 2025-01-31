df68f3604ad1f06b9411faff1734ca32897ee9f5
Cleiton barros <ton.barros123@hotmail.com>
2025-01-31 19:03:22 -0300

    feat: validation endpoint

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -9,11 +9,14 @@ import {
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dtos';
 import { AddressEntity } from './entities/address.entity';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @Roles(UserType.User)
   @Post('/:userId')
   @UsePipes(ValidationPipe)
   async createAdress(
